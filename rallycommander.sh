d0ec89559b08d280f02f5ec22afd5544e27cabfd
Daniel Levy <dlevy@us.ibm.com>
2016-01-13 11:10:49 -0600

    Add support for multiple runs

--- rallycommander.sh
+++ rallycommander.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-WAIT_TIME=10
+WAIT_TIME=5
 DEPLOYER="root@169.53.176.174"
 NMON_RUNTIME=300
 NMON_WINDOW=2
+NUMBER_OF_RUNS=2
 #Kill the program if we dont have correct arguemnts
 die () {
     echo >&2 "$@"
@@ -25,20 +26,27 @@ copynmon(){
     '" >> $1/$2.nmon
 }
 
+runrally(){
+	echo "Running rally tests"
+	cat rallyrun.sh | sed "s/REPLACE_VAL/$1/g"| ssh $DEPLOYER
+	scp $DEPLOYER:/root/rally-install/output$1.html $2/output$1
+	sleep $WAIT_TIME
+}
+echo Create results folder: $1
+mkdir $1
 echo "Running nmon on controller1 and controller2"
 ssh $DEPLOYER ssh controller1 nmon -fT -s $NMON_WINDOW -c $NMON_RUNTIME
 ssh $DEPLOYER ssh controller2 -f nmon -fT -s $NMON_WINDOW -c $NMON_RUNTIME
 echo "nmon has been started. Delaying $WAIT_TIME seconds before rally tests"
 sleep $WAIT_TIME
-echo "Running rally tests"
-cat rallyrun.sh | ssh $DEPLOYER
-sleep $WAIT_TIME
+for i in `seq 1 $NUMBER_OF_RUNS`;
+do
+    runrally $i $1
+done  
 echo "Killing nmon1 and nmon2"
 killnmon controller1
 killnmon controller2
-echo "Copying over result data to: $1"
-mkdir $1
-scp $DEPLOYER:/root/rally-install/output.html $1
+
 copynmon $1 controller1
 copynmon $1 controller2
 echo "Process complete"
--- rallyrun.sh
+++ rallyrun.sh
@@ -3,6 +3,7 @@ export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=/root/development
 source /usr/local/bin/virtualenvwrapper.sh
 workon rally
+echo "Passed in value---- REPLACE_VAL" REPLACE_VAL
 rally task start /root/rally-install/rally/samples/tasks/scenarios/vm/boot-ping-ssh-vm-share-network.yaml
 #Gets the last rally task id
-rally task report $(rally task list | cut -d' ' -f2| tail -2 | head -n 1) --out /root/rally-install/output.html
+rally task report $(rally task list | cut -d' ' -f2| tail -2 | head -n 1) --out /root/rally-install/outputREPLACE_VAL.html
