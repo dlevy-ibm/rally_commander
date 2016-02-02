ce828ea29d200851ed332da63699e4b9bf7ed15b
Daniel Levy <dlevy@us.ibm.com>
2016-02-01 18:29:58 -0600

    Add support for top recording Switch to mexico environment

--- rallycommander.sh
+++ rallycommander.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WAIT_TIME=150
-DEPLOYER="root@169.53.176.174"
+DEPLOYER="root@169.57.123.165"
 NMON_RUNTIME=30000
 NMON_WINDOW=2
 NUMBER_OF_RUNS=4
@@ -13,28 +13,30 @@ die () {
 [ "$#" -eq 1 ] || die "1 argument required (output directory), $# provided"
 
 #Kill the nmon process
-killnmon() {
+killprocess() {
    ssh $DEPLOYER ssh $1 "'
-       pgrep -f nmon | xargs kill -9
+       pgrep -f $2 | xargs kill -9
    '"
 }
 
 #Copy the nmon files
 copynmon(){
-  # ssh $DEPLOYER ssh $2 "'
-  #     ls -t | grep .*nmon | head -n1 | xargs -r cat
-  #  '" >> $1/$2.nmon
   ssh $DEPLOYER ssh $2 "'
-        cat run$i.nmom
+        cat run$3.nmon
   '" >> $1/$2.run$3.nmon
+}
 
+#Copy the nmon files
+copytop(){
+  ssh $DEPLOYER ssh $2 "'
+        cat /root/run$3.top
+  '" >> $1/$2.run$3.top
 }
 
 runrally(){
 	echo "Running rally tests"
 	cat rallyrun.sh | sed "s/REPLACE_VAL/$1/g"| ssh $DEPLOYER
 	scp $DEPLOYER:/root/rally-install/output$1.html $2/output$1.html
-	
 }
 
 
@@ -43,19 +45,27 @@ echo Create results folder: $1
 mkdir $1
 for i in `seq 1 $NUMBER_OF_RUNS`;
 do
-    echo "Running nmon on controller1 and controller2"
-    ssh $DEPLOYER ssh controller1 nmon -fT -s $NMON_WINDOW -c $NMON_RUNTIME -F run$i.nmon
-    ssh $DEPLOYER ssh controller2 -f nmon -fT -s $NMON_WINDOW -c $NMON_RUNTIME -F run$i.nmon
-    #echo "nmon has been started. Delaying $WAIT_TIME seconds before rally tests"
+    echo "---- Beginning run $i of $NUMBER_OF_RUNS---"
+    echo "Running nmon and top on controller1 and controller2"
+    ssh $DEPLOYER ssh controller1 nmon -fT -s $NMON_WINDOW -c $NMON_RUNTIME -F /root/run$i.nmon
+    ssh $DEPLOYER ssh controller2 nmon -fT -s $NMON_WINDOW -c $NMON_RUNTIME -F /root/run$i.nmon
+    ssh $DEPLOYER -f -n ssh controller1 -f -n top -d 4 -b -o USER >> $1/controller1.run$i.top
+    ssh $DEPLOYER -f -n ssh controller2 -f -n top -d 4 -b -o USER >> $1/controller2.run$i.top
+    echo "Sleep for 10 (s)"
     sleep 10
+    #ssh $DEPLOYER ssh controller1 top -b -d 10 > top$i.txt
     runrally $i $1
     echo "Waiting $WAIT_TIME (s) before next test run..."
     sleep $WAIT_TIME
     echo "Killing nmon1 and nmon2"
-    killnmon controller1
-    killnmon controller2
+    killprocess controller1 nmon
+    killprocess controller2 nmon
+    killprocess controller1 top
+    killprocess controller2 top
     copynmon $1 controller1 $i
     copynmon $1 controller2 $i
+    #copytop $1 controller1 $i
+    #copytop $1 controller2 $i
 done  
 
 echo "Process complete"
